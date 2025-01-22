a85fd573f46f31483b0150eab1b24aad8731e61b
H-Loong <2638102494@qq.com>
2025-01-22 15:03:05 +0800

    图片管理模块

--- src/main/java/com/hloong/intelligallery/service/PictureService.java
+++ src/main/java/com/hloong/intelligallery/service/PictureService.java
@@ -1,11 +1,16 @@
 package com.hloong.intelligallery.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hloong.intelligallery.model.domain.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hloong.intelligallery.model.domain.User;
+import com.hloong.intelligallery.model.dto.picture.PictureQueryRequest;
 import com.hloong.intelligallery.model.dto.picture.PictureUploadRequest;
 import com.hloong.intelligallery.model.vo.PictureVO;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author lenovo
@@ -16,4 +21,11 @@ public interface PictureService extends IService<Picture> {
 
     PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser);
 
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    void validPicture(Picture picture);
 }
